e86faa2a2c1d2822d364ca0f410795e1f756bfc7
Kristoffer <kristofferpudas@gmail.com>
2019-03-29 00:40:37 +0100

    Adjust canvas size on window resize

--- src/client/View.js
+++ src/client/View.js
@@ -5,9 +5,25 @@ export default class View {
     this.width = width
     this.height = height
     this.ctx = canvas.getContext('2d')
+    this.adjustCanvasSize = this.adjustCanvasSize.bind(this)
+    this.init()
+  }
+
+  init() {
+    this.adjustCanvasSize()
+    this.adjustCanvasSizeAsNeeded()
   }
 
   clear() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+  }
+
+  adjustCanvasSizeAsNeeded() {
+    addEventListener('resize', this.adjustCanvasSize)
+  }
+
+  adjustCanvasSize() {
+    this.canvas.width = window.innerWidth
+    this.canvas.height = window.innerHeight
   }
 }
--- src/client/index.js
+++ src/client/index.js
@@ -3,10 +3,10 @@ import Space from './Space'
 import Planet from './Planet'
 
 function run() {
-  const game = new SpaceGame(1024, 768)
+  const game = new SpaceGame(1024, 720)
   const space = new Space()
 
-  const planet = new Planet(1024/2, 768/2, 20)
+  const planet = new Planet(1024/2, 720/2, 20)
 
   space.add(planet)
 

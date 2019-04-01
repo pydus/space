0c038376c976f28b34f2315c974bfbf8d48c8581
Kristoffer <kristofferpudas@gmail.com>
2019-04-01 18:37:00 +0200

    Set camera min width

--- src/client/CameraController.js
+++ src/client/CameraController.js
@@ -1,10 +1,14 @@
-export default ({ p, camera, velFactor = 0.04 }) => ({
-  originalWidth: camera.width,
+export default ({ p, camera, velFactor = 0.04, minWidth = p.rad * 4 }) => ({
   velFactor,
+  minWidth,
+
+  originalWidth: camera.width,
 
   updateWidth: function() {
     const vel = Math.abs(p.vel.x) + Math.abs(p.vel.y)
-    camera.setWidth(this.originalWidth * (1 + vel * this.velFactor))
+    const width = this.originalWidth * (1 + vel * this.velFactor)
+    const boundWidth = Math.max(this.minWidth, width)
+    camera.setWidth(boundWidth)
   },
 
   updatePosition: function() {
